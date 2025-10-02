d31c6164aa6122271c983cb0bc23e1244b048480
ANJALI <anjalibalaji7@gmail.com>
2025-10-02 13:27:40 +0530

    Second commit through git

---
+++ lambdaproj/src/lambdaproj/Lambdapro.java
@@ -0,0 +1,16 @@
+package lambdaproj;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Lambdapro {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Administrator\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+		driver.findElement(By.name("email")).sendKeys("abc2gmail.com");
+   	 driver.findElement(By.name("password")).sendKeys("abcdefg");
+	}
+}
